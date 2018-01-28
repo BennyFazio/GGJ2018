a44ec230b1fef8dd85443f4be71d82f1929261a5
Your Name <rikzander94@gmail.com>
2018-01-28 14:19:21 -0500

    adfd

--- scripts/global.gd
+++ scripts/global.gd
@@ -28,6 +28,7 @@ var player_minjump = -200
 #Mech 1
 var mech_1_pos = Vector2()
 var mech_2_pos = Vector2()
+var currentScene = null
 
 
 
@@ -45,6 +46,23 @@ var zeus_jumpspeed = -1800
 var zeus_minjump = -200
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
+	   #On load set the current scene to the last scene available
+   currentScene = get_tree().get_root().get_child(get_tree().get_root().get_child_count() -1)
+   #Demonstrate setting a global variable.
+   Globals.set("MAX_POWER_LEVEL",9000)
+
+
+   
+# create a function to switch between scenes 
+func setScene(scene):
+   #clean up the current scene
+   currentScene.queue_free()
+   #load the file passed in as the param "scene"
+   var s = ResourceLoader.load(scene)
+   #create an instance of our scene
+   currentScene = s.instance()
+   # add scene to root
+   get_tree().get_root().add_child(currentScene)
+
+	
+	
